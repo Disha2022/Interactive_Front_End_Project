f134c0f469ff969b6ef594ebe548022f2b2ac7af
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-04-02 14:36:05 -0400

    created modal confirming search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,19 @@
-var overlay = $('overlay')
-var 
+var overlay = $('#overlay')
+var closeBtn = $('#close-modal')
+var searchBtn = $('#search-btn')
+var searchForm = $('#search-form')
+var cancelBtn = $('#cancel-btn')
+
+searchForm.submit(function(e){
+    e.preventDefault();
+})
+
+function AppearModal(){
+        overlay.removeClass('hidden').addClass('flex')
+}
+function DissapearModal(){
+    overlay.removeClass('flex').addClass('hidden')
+}
+searchBtn.on('click', AppearModal)
+closeBtn.on('click', DissapearModal)
+cancelBtn.on('click', DissapearModal)
