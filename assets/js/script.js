b4c7d363fbb2809cfd2be35a71c0680151053b87
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-04-03 15:43:31 -0400

    created geocoder to change lat/lng based on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,26 @@ var closeBtn = $('#close-modal');
 var searchBtn = $('#search-btn');
 var searchForm = $('#search-form');
 var cancelBtn = $('#cancel-btn');
+var confirmBtn = $('#confirm-btn');
 var searchInput = document.getElementById('user-search');
-
-const request ={
-    location: new google.maps.LatLng(51.5287352, -0.3817841),
+var request ={
+    location: {lat: 28.6024, lng: -81.2001},
     radius: 5000,
     type: ['restaurant']
 };
-const results = [];
-const places = document.getElementById('places')
-const service = new google.maps.places.PlacesService(places);
 
-service.nearbySearch(request, callback);
 
-function callback(response, status, pagination) {
+function searchForAddress(){
+
+
+    const results = [];
+    const places = document.getElementById('places')
+
+
+    const service = new google.maps.places.PlacesService(places);
+    service.nearbySearch(request, callback);
+
+    function callback(response, status, pagination) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         results.push(...response);
     }
@@ -25,23 +31,40 @@ function callback(response, status, pagination) {
         setTimeout(() => pagination.nextPage(), 2000);
     } else {
         displayResults();
+        DissapearModal();
+        searchInput.value=''
     }
 }
 
-function displayResults(){
+
+    function displayResults(){
     results.filter(result => result.rating)
     .sort((a, b) => a.rating > b.rating ? -1 : 1)
     .forEach(result => {
         places.innerHTML += `<li>${result.name} - ${result.rating}</li>`
     })
 }
- 
-function addSearchInput(){
-    var Location= searchInput.value
-
 }
 
-searchForm.submit(function(e){
+function initialize(){
+    geocoder = new google.maps.Geocoder();
+}
+
+function codeAddress() {
+    var address = searchInput.value;
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') {
+
+       request.location.lat= results[0].geometry.location.lat()
+       request.location.lng= results[0].geometry.location.lng()
+      }
+      else{
+          alert('Error: Please Enter Valid Address')
+      }
+    });
+}
+
+    searchForm.submit(function(e){
     e.preventDefault();
 })
 
@@ -52,9 +75,16 @@ function DissapearModal(){
     overlay.removeClass('flex').addClass('hidden')
 }
 
+function insertModalAddress(){
+    var modalText = searchInput.value
+    document.getElementById('modal-text').textContent='Is ' + modalText + ' the correct address?'
+}
 
-searchInput.addEventListener('keyup', addSearchInput)
+
+searchInput.addEventListener('keyup', insertModalAddress)
+confirmBtn.on('click', searchForAddress)
 searchBtn.on('click', AppearModal)
+searchBtn.on('click', codeAddress)
 closeBtn.on('click', DissapearModal)
 cancelBtn.on('click', DissapearModal)
 
